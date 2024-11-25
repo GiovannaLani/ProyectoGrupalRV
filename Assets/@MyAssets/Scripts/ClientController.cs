197dab33292fd2530c507688e91d83b1c8463ce8
PatriciaRdrz <patricia.r@opendeusto.es>
2024-11-25 17:11:51 +0100

    Update de pedido

--- Assets/@MyAssets/Scripts/ClientController.cs
+++ Assets/@MyAssets/Scripts/ClientController.cs
@@ -8,6 +8,88 @@ public class ClientController : MonoBehaviour
 
     void Start()
     {
+<<<<<<< Updated upstream
+=======
+        agent = GetComponent<NavMeshAgent>();
+        pointsToVisit = Random.Range(2, patrolPoints.Count + 1);
+        animator = GetComponent<Animator>();
+        StartCoroutine(MoveToPoints());
+    }
+    IEnumerator MoveToPoints()
+    {
+        List<Transform> visitedPoints = new List<Transform>();
+
+        while (isAlive)
+        {
+            if (isFinalMove)
+            {
+                currentTarget = finalPoints[Random.Range(0, finalPoints.Count)];
+                agent.SetDestination(currentTarget.position);
+
+                while (agent.pathPending || agent.remainingDistance > agent.stoppingDistance)
+                {
+                    animator.SetBool("lookAround", false);
+                    animator.SetBool("walk", true);
+
+                    if (agent.velocity.sqrMagnitude > 0.01f)
+                    {
+                        Vector3 direction = agent.velocity.normalized; 
+                        Quaternion lookRotation = Quaternion.LookRotation(direction * -1);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f); 
+                    }
+                    yield return null;
+                }
+
+                animator.SetBool("walk", false);
+                StopMovement();
+                yield break;
+            }
+
+            if (!isGoingToBuy && Random.value < 0.5f && buyPoint != null && pointsVisited < pointsToVisit - 1)
+            {
+                currentTarget = buyPoint;
+                waitTime = 50f;
+                isGoingToBuy = true;
+                pointsVisited++;
+            }
+            else
+            {
+                do
+                {
+                    currentTarget = patrolPoints[Random.Range(0, patrolPoints.Count)];
+                } while (visitedPoints.Contains(currentTarget));
+
+                visitedPoints.Add(currentTarget);
+                pointsVisited++;
+            }
+
+            agent.SetDestination(currentTarget.position);
+
+            while (agent.pathPending || agent.remainingDistance > agent.stoppingDistance)
+            {
+                animator.SetBool("lookAround", false);
+                animator.SetBool("walk", true);
+
+                    if (agent.velocity.sqrMagnitude > 0.01f) 
+                {
+                    Vector3 direction = agent.velocity.normalized;
+                    Quaternion lookRotation = Quaternion.LookRotation(direction*-1);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+                }
+                yield return null;
+            }
+
+            animator.SetBool("walk", false);
+
+            agent.updateRotation = true;
+            animator.SetBool("lookAround",true);
+            yield return new WaitForSeconds(waitTime);
+            if (pointsVisited >= pointsToVisit || isGoingToBuy)
+            {
+                isFinalMove = true;
+            }
+        }
+>>>>>>> Stashed changes
     }
 
     void Update()
--- Assets/@MyAssets/Scripts/OrderController.cs
+++ Assets/@MyAssets/Scripts/OrderController.cs
@@ -6,6 +6,11 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class OrderController : MonoBehaviour
 {
     private XRSocketInteractor socketBox;
+<<<<<<< Updated upstream
+=======
+    private ClientController client;
+    private BoxController boxController;
+>>>>>>> Stashed changes
     public string boxTag = "Box";
     public GameObject counterPosition; //posicion en la que esperan los clientes
     private bool isBoxOnSocket;
@@ -23,11 +28,30 @@ public class OrderController : MonoBehaviour
     }
 
     private void OnBoxOnSocket(SelectEnterEventArgs args)
+<<<<<<< Updated upstream
     {//comprobar que la caja esta bien puesta con el producto adecuado
         if (args.interactableObject.transform.CompareTag(boxTag))
         {
             isBoxOnSocket = true;
             Debug.Log("Caja en socket");
+=======
+    {
+
+        if (args.interactableObject.transform.CompareTag(boxTag))
+        {
+            boxController = args.interactableObject.transform.GetComponent<BoxController>();
+            //isBoxOnSocket = true;
+            Debug.Log(boxController.IsReadyForDelivery());
+            if (client.isOnBuyPoint() && boxController.IsReadyForDelivery())
+            {
+                Debug.Log("Objecto se ha destruido");
+                var lidSocket = boxController.socketLid.transform.gameObject;
+                var contentSocket = boxController.socketContent.transform.gameObject;
+                Destroy(args.interactableObject.transform.gameObject);
+                Destroy(lidSocket);
+                Destroy(contentSocket);
+            }
+>>>>>>> Stashed changes
         }
     }
     /*
